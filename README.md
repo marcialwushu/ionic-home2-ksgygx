edc41c0fe065aa088014d8aa6c090914e5160b0f
marcialwushu <marcialwushu@gmail.com>
2019-08-25 22:55:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-home2-ksgygx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-home2-ksgygx)
---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+// import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  // rootPage:any = TabsPage;
+
+  rootPage : any = HomePage;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+
+    });
+  }
+}
